e8075930be71bb0d46e725b070c637b2af444e3a
59naga <i59naga@icloud.com>
2019-06-10 06:07:05 +0900

    :wrench: 圧縮＋ソースマップ形式のリリースに変更

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/index.mjs',
   output: {
-    file: 'dist/finder.js',
+    file: 'dist/finder.min.js',
     format: 'umd',
-    name: 'kotodamanFinder'
+    name: 'kotodamanFinder',
+    sourcemap: true,
+    sourcemapFile: 'dist/finder.min.js.map'
   },
   plugins: [
     commonjs({
@@ -15,5 +18,6 @@ export default {
     }),
     resolve(),
     json(),
+    terser(),
   ]
 };
